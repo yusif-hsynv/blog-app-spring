f46c6bda55ec06ae3e93d7f46396ebefda05491c
Yusif Huseynov <yusif2415@gmail.com>
2025-11-13 22:40:58 +0400

    Update CategoryRepository.java

--- src/main/java/az/orient/blogapp/repository/CategoryRepository.java
+++ src/main/java/az/orient/blogapp/repository/CategoryRepository.java
@@ -10,6 +10,7 @@ import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
     @Query("SELECT c FROM Category c LEFT JOIN FETCH c.posts")
     List<Category> findAllWithPostCount();
 
